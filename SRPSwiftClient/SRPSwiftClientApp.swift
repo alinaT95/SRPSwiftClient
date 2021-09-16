cda6b4221ee807b98ad2522e20d366a45bd14895
Alina Trepacheva <alina1989malina@yandex.ru>
2021-09-16 12:53:15 +0300

    Initial Commit

---
+++ SRPSwiftClient/SRPSwiftClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  SRPSwiftClientApp.swift
+//  SRPSwiftClient
+//
+//  Created by Alina Alinovna on 16.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct SRPSwiftClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
